1812a06f006bd85f5ac7f0793d08d3a9e7f7d41f
Zehlan1 <92109535+Zehlan1@users.noreply.github.com>
2022-12-19 13:18:26 +0100

    Initial commit
    Created a template for the application

---
+++ ASP.NET_Uni_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Uni_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
